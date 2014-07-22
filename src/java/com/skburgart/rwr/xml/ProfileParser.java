8c11c5eebb8feb584f95b26ca1802bd63a390065
Steven Burgart <skburgart@gmail.com>
2014-07-22 11:30:47 -0400

    Get basic user info

--- src/java/com/skburgart/rwr/xml/ProfileParser.java
+++ src/java/com/skburgart/rwr/xml/ProfileParser.java
@@ -6,6 +6,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -15,11 +18,16 @@ import org.xml.sax.SAXException;
 public class ProfileParser {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        File fXmlFile = new File("C:\\Users\\Steven\\Documents\\NetBeansProjects\\RWR Stats\\profiles\\485161409.person");
+
+        File fXmlFile = new File("C:\\Users\\Steven\\Documents\\NetBeansProjects\\RWR Stats\\profiles\\485161409.profile");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(fXmlFile);
 
-        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+        Element person = (Element) doc.getElementsByTagName("profile").item(0);
+        
+        System.out.println("username -> " + person.getAttribute("username"));
+        System.out.println("game_version -> " + person.getAttribute("game_version"));
+        System.out.println("digest -> " + person.getAttribute("digest"));
     }
 }
