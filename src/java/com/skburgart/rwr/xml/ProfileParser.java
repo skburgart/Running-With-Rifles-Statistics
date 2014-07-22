e15bca51b7968654a324f2d31adcc2d8c0bc1975
Steven Burgart <skburgart@gmail.com>
2014-07-22 11:45:32 -0400

    Parse person xml

--- src/java/com/skburgart/rwr/xml/ProfileParser.java
+++ src/java/com/skburgart/rwr/xml/ProfileParser.java
@@ -7,8 +7,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -19,26 +17,41 @@ public class ProfileParser {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
 
-        File fXmlFile = new File("C:\\Users\\Steven\\Documents\\NetBeansProjects\\RWR Stats\\profiles\\485161409.profile");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
 
-        Element person = (Element) doc.getElementsByTagName("profile").item(0);
-        System.out.println("username -> " + person.getAttribute("username"));
-        System.out.println("game_version -> " + person.getAttribute("game_version"));
-        System.out.println("digest -> " + person.getAttribute("digest"));
+        File profileXMLFile = new File("C:\\Users\\Steven\\Documents\\NetBeansProjects\\RWR Stats\\profiles\\485161409.profile");
+        File personXMLFile = new File("C:\\Users\\Steven\\Documents\\NetBeansProjects\\RWR Stats\\profiles\\485161409.person");
+        Document profileXML = dBuilder.parse(profileXMLFile);
+        Document personXML = dBuilder.parse(personXMLFile);
 
-        Element stats = (Element) doc.getElementsByTagName("stats").item(0);
-        System.out.println("time_played -> " + stats.getAttribute("time_played"));
-        System.out.println("kills -> " + stats.getAttribute("deaths"));
-        System.out.println("deaths -> " + stats.getAttribute("kills"));
-        System.out.println("player_kills -> " + stats.getAttribute("player_kills"));
-        System.out.println("teamkills -> " + stats.getAttribute("teamkills"));
-        System.out.println("longest_kill_streak -> " + stats.getAttribute("longest_kill_streak"));
-        System.out.println("targets_destroyed -> " + stats.getAttribute("targets_destroyed"));
-        System.out.println("vehicles_destroyed -> " + stats.getAttribute("vehicles_destroyed"));
-        System.out.println("soldiers_healed -> " + stats.getAttribute("soldiers_healed"));
-        System.out.println("times_got_healed -> " + stats.getAttribute("times_got_healed"));
+        Element profile = (Element) profileXML.getElementsByTagName("profile").item(0);
+        System.out.println("username -> " + profile.getAttribute("username"));
+        System.out.println("game_version -> " + profile.getAttribute("game_version"));
+        System.out.println("digest -> " + profile.getAttribute("digest"));
+
+        Element profile_stats = (Element) profileXML.getElementsByTagName("stats").item(0);
+        System.out.println("time_played -> " + profile_stats.getAttribute("time_played"));
+        System.out.println("kills -> " + profile_stats.getAttribute("deaths"));
+        System.out.println("deaths -> " + profile_stats.getAttribute("kills"));
+        System.out.println("player_kills -> " + profile_stats.getAttribute("player_kills"));
+        System.out.println("teamkills -> " + profile_stats.getAttribute("teamkills"));
+        System.out.println("longest_kill_streak -> " + profile_stats.getAttribute("longest_kill_streak"));
+        System.out.println("targets_destroyed -> " + profile_stats.getAttribute("targets_destroyed"));
+        System.out.println("vehicles_destroyed -> " + profile_stats.getAttribute("vehicles_destroyed"));
+        System.out.println("soldiers_healed -> " + profile_stats.getAttribute("soldiers_healed"));
+        System.out.println("times_got_healed -> " + profile_stats.getAttribute("times_got_healed"));
+
+        Element person = (Element) personXML.getElementsByTagName("person").item(0);
+        System.out.println("max_authority_reached -> " + person.getAttribute("max_authority_reached"));
+        System.out.println("authority -> " + person.getAttribute("authority"));
+        System.out.println("job_points -> " + person.getAttribute("job_points"));
+        System.out.println("faction -> " + person.getAttribute("faction"));
+        System.out.println("name -> " + person.getAttribute("name"));
+        System.out.println("version -> " + person.getAttribute("version"));
+        System.out.println("soldier_group_id -> " + person.getAttribute("soldier_group_id"));
+        System.out.println("block -> " + person.getAttribute("block"));
+        System.out.println("squad_size_setting -> " + person.getAttribute("squad_size_setting"));
+        System.out.println("squad_config_index -> " + person.getAttribute("squad_config_index"));
     }
 }
