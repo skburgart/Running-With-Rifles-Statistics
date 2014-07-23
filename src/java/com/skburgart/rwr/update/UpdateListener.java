68676064e0edbb4b3171207d1cabc24c14172247
Steven Burgart <skburgart@gmail.com>
2014-07-22 22:00:55 -0400

    Add update files

---
+++ src/java/com/skburgart/rwr/update/UpdateListener.java
@@ -0,0 +1,31 @@
+package com.skburgart.rwr.update;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ *
+ * @author Steven Burgart <skburgart@gmail.com>
+ */
+public class UpdateListener implements ServletContextListener {
+
+    private static final int UPDATE_INTERVAL_MINS = 5;
+    private volatile ScheduledExecutorService executor;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        executor = Executors.newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(new UpdateRunnable(), 0, UPDATE_INTERVAL_MINS, TimeUnit.MINUTES);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (executor != null) {
+            executor.shutdown();
+            this.executor = null;
+        }
+    }
+}
---
+++ src/java/com/skburgart/rwr/update/UpdateRunnable.java
@@ -0,0 +1,36 @@
+package com.skburgart.rwr.update;
+
+import com.skburgart.rwr.RWRConfig;
+import com.skburgart.rwr.vo.Player;
+import static com.skburgart.rwr.xml.PlayerParser.parseDirectory;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.xml.parsers.ParserConfigurationException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author Steven Burgart <skburgart@gmail.com>
+ */
+public class UpdateRunnable implements Runnable {
+
+    @Override
+    public void run() {
+
+        try {
+            SessionFactory factory = new Configuration().configure().buildSessionFactory();
+            Session session = factory.openSession();
+            session.beginTransaction();
+            ArrayList<Player> players = parseDirectory(RWRConfig.get("profiles.dir"));
+            for (Player p : players) {
+                session.saveOrUpdate(p);
+            }
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
